dd69a0fca9e622c31cfe04f1257480d111ee57fb
Peter Eisentraut <peter@eisentraut.org>
2019-08-24 12:20:20 +0200

    Add a TODO test

--- src/test/recovery/t/018_basebackup.pl
+++ src/test/recovery/t/018_basebackup.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use PostgresNode;
 use TestLib;
-use Test::More tests => 1;
+use Test::More tests => 2;
 
 my $node1 = get_new_node('node1');
 $node1->init(allows_streaming => 1);
@@ -15,6 +15,7 @@ $node1->safe_psql('postgres',
 my $node2 = get_new_node('node2');
 $node2->init(allows_streaming => 1, extra => [ '--replica' ]);
 $node2->append_conf('postgresql.conf', "primary_conninfo = '" . $node1->connstr . "'");
+my $old_mtime = (stat($node2->data_dir . '/postgresql.conf'))[9];
 $node2->start;
 
 $node1->wait_for_catchup($node2, 'replay', $node1->lsn('insert'));
@@ -22,3 +23,10 @@ $node1->wait_for_catchup($node2, 'replay', $node1->lsn('insert'));
 is($node2->safe_psql('postgres', "SELECT count(*) FROM tab_int"),
    qq(1000),
    'check content of standby');
+
+TODO: {
+local $TODO = 'not yet implementated';
+my $new_mtime = (stat($node2->data_dir . '/postgresql.conf'))[9];
+is($new_mtime, $old_mtime,
+   'configuration files were not copied');
+};
